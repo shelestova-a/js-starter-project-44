63e80dfaa90483970174b4b1ae7ec2e309a7b2d3
Anna Shelestova <anna.shelestova.1@gmail.com>
2022-12-14 18:10:13 +0100

    add src/games/progression.js

---
+++ src/games/progression.js
@@ -0,0 +1,33 @@
+import readlineSync from 'readline-sync';
+
+import * as index from '../index.js';
+
+export const callProgression = () => {
+  const name = index.greeting();
+  console.log('What number is missing in the progression?');
+  let i = 0;
+
+  while (i < index.max) {
+    let number1 = Math.floor(Math.random() * 100);
+    const difference = Math.floor(Math.random() * 100);
+    const result = [number1];
+    const indexMissed = Math.floor(Math.random() * 10);
+    let j = 0;
+    while (j < 9) {
+      number1 += difference;
+      result.push(number1);
+      j += 1;
+    }
+    const correctAnswer = result[indexMissed];
+    result[indexMissed] = '..';
+    console.log(`Question: ${result}`);
+    const answer = readlineSync.question('Your answer: ');
+    if (answer * 1 === correctAnswer) {
+      i += 1;
+      console.log('Correct!');
+    } else {
+      return `'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'. \nLet's try again, ${name}!`;
+    }
+  }
+  return `Congratulations, ${name}!`;
+};
