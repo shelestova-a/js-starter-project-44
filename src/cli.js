01edd3b38a5d10b59c29c2628512fbffa50b733a
Anna Shelestova <anna.shelestova.1@gmail.com>
2022-12-14 02:09:05 +0100

    delete src/cli.js and src/even.js

--- src/cli.js
+++
@@ -1,7 +0,0 @@
-import readlineSync from 'readline-sync';
-
-export const greeting = () => {
-  console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name? ');
-  return (`Hello, ${name}!`);
-};
