e61692991df0b6c306ad71a68b83888bb28faeb9
Anna Shelestova <anna.shelestova.1@gmail.com>
2022-12-14 02:10:46 +0100

    add games (calc.js, cli.js, even.js) and index.js

---
+++ src/games/calc.js
@@ -0,0 +1,40 @@
+import readlineSync from 'readline-sync';
+
+import * as index from '../index.js';
+
+const operators = ['+', '-', '*'];
+
+export const count = (operator, number1, number2) => {
+  switch (operator) {
+  case operators[0]:
+    return number1 + number2;
+  case operators[1]:
+    return number1 - number2;
+  case operators[2]:
+    return number1 * number2;
+  default:
+    return null;
+  }
+};
+
+export const calculate = () => {
+  const name = index.greeting();
+  console.log('What is the result of the expression?');
+  let i = 0;
+
+  while (i < index.max) {
+    const number1 = Math.floor(Math.random() * 100);
+    const number2 = Math.floor(Math.random() * 100);
+    const operator = operators[Math.floor(Math.random() * 3)];
+    console.log(`Question: ${number1} ${operator} ${number2}`);
+    const answer = readlineSync.question('Your answer: ');
+    const correctAnswer = count(operator, number1, number2);
+    if (answer * 1 === correctAnswer) {
+      i += 1;
+      console.log('Correct!');
+    } else {
+      return `'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'. \nLet's try again, ${name}!`;
+    }
+  }
+  return `Congratulations, ${name}!`;
+};
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import readlineSync from 'readline-sync';
+
+export const greeting = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  return name;
+};
+
+export const random = Math.floor(Math.random() * 100);
+
+export const max = 3;
